020a4f6e83599a36d01d481c7a09365ce4a8fe9e
s7xWn <47253767+s7xWn@users.noreply.github.com>
2019-02-01 20:31:25 +0300

    Create index.php

---
+++ index.php
@@ -0,0 +1,67 @@
+<!DOCTYPE html>
+<html lang="ru">
+
+<head>
+	<meta charset="UTF-8">
+	<title>667 Welcome</title>
+	<meta name="viewport" content="initial-scale = 1.0, maximum-scale = 1.0, user-scalable = no, width = device-width" />
+	<link rel="stylesheet" type="text/css" href="css/style.css">
+</head>
+
+<body>
+	<header>
+		<p class = "name-rap">-s7xWn <span class="typed"></span></p>
+		<p class = "name-rap"><span class="typed1"></span></p>
+	</header>
+<div id="hellopreloader">
+	<div id="hellopreloader_preload"></div>
+	<div id="trailer" class="is_overlay">
+		 <video muted autoplay autoplay="autoplay" id="video" width="100%" height="auto" autoplay="autoplay" >
+			<source autoplay="autoplay" src="XXX.mp4"></source>
+		</video>
+		<div id="mask">
+			<div class="Center-html"></div>
+			<div id = "main">
+				<embed id="music" src="x.mp3" autostart="true" hidden="true" loop="false"> </embed>
+				<div id = "NickName">
+					<h2>6 6 7</h2>
+					 <div id = "Social">
+						<a class="Social" href="https://vk.com/mellerss322">VK </a>|
+						<a class="Social"  href="https://steamcommunity.com/id/trueBlick2">STEAM </a>|
+						<a class="Social" href="https://www.t.me/mellers0">TELEGRAM</a> |
+						<a class="Social" href="https://darkwebs.ws/members/59192/">DARKWEBS</a>
+					</div> 
+					<div class = "how1">
+						<p><span class="how">Look at me</span></p>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+
+
+
+
+	
+<script src="js/script.js"></script>
+<script type="text/javascript">
+var typed = new Typed(".typed", {
+// Waits 1000ms after typing "First"
+strings: ["1998 - 2018"], // Слова,которые будут писаться.
+typeSpeed:180, //Скорость написания.
+backSpeed:180, // Скорость стирания.
+loop:true //Создаёт цикл бесконечного повторения.
+});
+</script>
+<script type="text/javascript">
+var typed = new Typed(".typed1", {
+// Waits 1000ms after typing "First"
+strings: ["1996 - 2017"], // Слова,которые будут писаться.
+typeSpeed:180, //Скорость написания.
+backSpeed:180, // Скорость стирания.
+loop:true //Создаёт цикл бесконечного повторения.
+});
+</script>
+
+</body>
+</html>
